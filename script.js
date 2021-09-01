354299e3019ce7d5973397708b995ca5bf7d7312
Govind Jaiswal <govindj763@gmail.com>
2021-09-01 16:07:09 +0530

    few changes

--- script.js
+++ script.js
@@ -12,3 +12,8 @@ menubtn.onclick= function(){
         menu.src="imges/menu.png";
     }
 }
+// All animations will take exactly 500ms
+var scroll = new SmoothScroll('a[href*="#"]', {
+	speed: 1000,
+	speedAsDuration: true
+});
